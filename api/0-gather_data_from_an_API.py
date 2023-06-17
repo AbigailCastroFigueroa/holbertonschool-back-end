dde485181d1ad1b6e0ee56089c0be7847b91cf04
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-06-17 01:05:21 -0400

    Adding if condition for name equal name

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
 """Script to request information using an API."""
-"""
-In this module I practically modify the the strings used 
-to retrieve the data.
-"""
 
-"""Imported libraries above."""
-
-
-import json
 import requests
 import sys
-user_id = sys.argv[1]
-url = 'https://jsonplaceholder.typicode.com/user/' + user_id + '/todos'
-users = 'https://jsonplaceholder.typicode.com/users?id=' + user_id
-data = requests.get(url)
-info = data.json()
-users_names = requests.get(users)
-response = users_names.json()
-undone = 0
-done = 0
 
-for item in response:
-    username = item['name']
+if __name__ == '__main__':
 
-for items in info:
-    if items['completed'] is True:
-        done += 1
-    undone += 1
-print(f"Employee {username} is done with tasks ({done}/{undone}):")
+    user_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/user/' + user_id + '/todos'
+    users = 'https://jsonplaceholder.typicode.com/users?id=' + user_id
+    data = requests.get(url)
+    info = data.json()
+    users_names = requests.get(users)
+    response = users_names.json()
+    undone = 0
+    done = 0
 
-for items in info:
-    if items['completed'] is True:
-        task = items['title']
-        print(f"\t {task}")
+    for item in response:
+        username = item['name']
+
+    for items in info:
+        if items['completed'] is True:
+            done += 1
+        undone += 1
+    print(f"Employee {username} is done with tasks ({done}/{undone}):")
+
+    for items in info:
+        if items['completed'] is True:
+            task = items['title']
+            print(f"\t {task}")
